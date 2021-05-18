76553b558400baf767e8bdc616e789a96d06ad59
meyekkul@gmail.com <meyekkul@gmail.com>
2021-05-18 10:32:45 +0800

    add script welcome

--- welcome.sh
+++ welcome.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/zsh
 
 greeting="Welcome"
 user=$(whoami)
 day=$(date +%A)
 
 echo "$greeting back $user! Today is $day, which is the best day of the entire week!"
-echo "Your Bash shell version is: $BASH_VERSION. Enjoy!"
+echo "Your zsh shell version is: $ZSH_VERSION. Enjoy!"
 
